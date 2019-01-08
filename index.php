2a8413b950c9cf1fd1886d13a1d56266f7db6fc5
Dmitry Lebedev <azzode@yandex.ru>
2019-01-08 03:43:07 +0300

    Olala

---
+++ index.php
@@ -0,0 +1,27 @@
+<!--?php
+	if(empty($_GET['color'])){
+		echo "<div style=\"background-color: lightgreen; width: 200px; height: 200px; margin-left: 0px;\"></div>";
+	}else{
+		echo "<div style=\"background-color:" . $_GET['color'] . "; width: 200px; height: 200px; margin-left: 0px;\"></div>";
+	}
+?-->
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8" />
+	  <meta name="description" content="TEEEEST!!!" />
+	  <meta http-equiv="description" content="TEEEEST!!!" />
+	  <link rel="stylesheet" type="text/css" href="css/style.css" />
+	  <title>Veslo</title>
+
+	  <script src="js/script.js"></script>
+	</head>
+	<body>
+		<div id="page">
+				<!--img style="width: 500px;" src="imgg.jpg"-->
+		</div>
+	</body>
+</html>
